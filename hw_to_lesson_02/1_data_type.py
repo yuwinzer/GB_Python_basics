dd81953e6d1fd60820ddcb5bf65c68c378a1f4b0
yuwinzer <75756616+yuwinzer@users.noreply.github.com>
2021-01-23 00:43:05 +0200

    Delete 1_data_type.py
    wrong file

--- hw_to_lesson_02/1_data_type.py
+++
@@ -1,4 +0,0 @@
-something = ['apple', 33, [54, 56, 76], 54, (11, 34, 7)]
-print(f"Убедимся, что был создан список: {type(something)}")
-for i in range (0, len(something)):
-    print(f"Элемент {something[i]} имеет тип данных: {type(something[i])}")
